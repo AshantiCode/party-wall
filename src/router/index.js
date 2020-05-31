fc54d2e45261bf426069ec475b5f3c138cdc2abf
Ashanti Haring <ashanti.haring@gmail.com>
2020-05-31 19:23:04 +0200

    import home

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Home from "../components/Home";
 import AddItem from "../components/AddItem";
 import SignIn from "../components/SignIn";
 import SignUp from "../components/SignUp";
