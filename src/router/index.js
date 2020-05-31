060c585d44cc441ad7c237dc10796bc4dedbd310
Ashanti Haring <ashanti.haring@gmail.com>
2020-05-31 19:06:46 +0200

    initialise routes index.js

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import AddItem from "../components/AddItem";
+import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: "Home",
+    },
+    {
+      path: "/add-item",
+      name: "add-item",
+      component: "AddItem",
+    },
+    {
+      path: "/sign-in",
+      name: "sign-in",
+      component: "SignIn",
+    },
+    {
+      path: "/sign-up",
+      name: "sign-up",
+      component: "SignUp",
+    },
+  ],
+});
