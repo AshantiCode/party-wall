a13b775eaef5a6d563be99a9419b096c9616b03a
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-01 20:59:35 +0200

    put component data into store, create state and methods

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,25 +2,33 @@
   <v-container>
     <v-row>
       <v-col sm="6" offset-sm="3">
-        <v-card v-for="(food) in food" :key="food.id" class="mb-5">
+        <v-card v-for="food in food" :key="food.id" class="mb-5">
           <v-container>
             <v-row>
               <v-col cols="5">
                 <v-img :src="require('@/assets/food.jpg')"></v-img>
               </v-col>
               <v-col cols="7">
-                <v-card-title>{{food.name}}</v-card-title>
+                <v-card-title>{{ food.name }}</v-card-title>
                 <v-card-text>
                   <v-expansion-panels accordion>
                     <v-expansion-panel>
-                      <v-expansion-panel-header>Description</v-expansion-panel-header>
-                      <v-expansion-panel-content>{{food.description}}</v-expansion-panel-content>
+                      <v-expansion-panel-header
+                        >Description</v-expansion-panel-header
+                      >
+                      <v-expansion-panel-content>{{
+                        food.description
+                      }}</v-expansion-panel-content>
                     </v-expansion-panel>
                   </v-expansion-panels>
                 </v-card-text>
                 <div>
-                  <v-chip class="ma-2" color="secondary">{{food.quantity}} x in Stock</v-chip>
-                  <v-chip class="ma-2" color="secondary">{{food.price}} $</v-chip>
+                  <v-chip class="ma-2" color="secondary"
+                    >{{ food.quantity }} x in Stock</v-chip
+                  >
+                  <v-chip class="ma-2" color="secondary"
+                    >{{ food.price }} $</v-chip
+                  >
                 </div>
               </v-col>
             </v-row>
@@ -35,11 +43,15 @@
                 <v-img :src="require('@/assets/drinks.jpg')"></v-img>
               </v-col>
               <v-col cols="7">
-                <v-card-title>{{drink.name}}</v-card-title>
+                <v-card-title>{{ drink.name }}</v-card-title>
 
                 <div>
-                  <v-chip class="ma-2" color="secondary">{{drink.quantity}} pack</v-chip>
-                  <v-chip class="ma-2" color="secondary">{{drink.price}} $</v-chip>
+                  <v-chip class="ma-2" color="secondary"
+                    >{{ drink.quantity }} pack</v-chip
+                  >
+                  <v-chip class="ma-2" color="secondary"
+                    >{{ drink.price }} $</v-chip
+                  >
                 </div>
               </v-col>
             </v-row>
@@ -53,41 +65,13 @@
 
 <script>
 export default {
-  data() {
-    return {
-      food: [
-        {
-          id: "9340923842",
-          name: "Steak",
-          description: "So lecker yummy yummy",
-          quantity: "3",
-          price: "5,99"
-        },
-        {
-          id: "932429034",
-          name: "Pizza",
-          description: "So lecker yummy yummy",
-          quantity: "10",
-          price: "8,99"
-        }
-      ],
-      drinks: [
-        {
-          id: "098938789378",
-          name: "Beer",
-          description: "I am thirsty",
-          quantity: "6",
-          price: "3,99"
-        },
-        {
-          id: "87739472",
-          name: "Coca Cola",
-          description: "I am thirsty",
-          quantity: "4",
-          price: "7,99"
-        }
-      ]
-    };
-  }
+  computed: {
+    food() {
+      return this.$store.getters.loadedFood;
+    },
+    drinks() {
+      return this.$store.getters.loadedDrinks;
+    },
+  },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,52 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-  state: {},
+  state: {
+    loadedFood: [
+      {
+        id: "9340923842",
+        name: "Steak",
+        description: "So lecker yummy yummy",
+        quantity: "3",
+        price: "5,99",
+      },
+      {
+        id: "932429034",
+        name: "Pizza",
+        description: "So lecker yummy yummy",
+        quantity: "10",
+        price: "8,99",
+      },
+    ],
+    loadedDrinks: [
+      {
+        id: "098938789378",
+        name: "Beer",
+        description: "I am thirsty",
+        quantity: "6",
+        price: "3,99",
+      },
+      {
+        id: "87739472",
+        name: "Coca Cola",
+        description: "I am thirsty",
+        quantity: "4",
+        price: "7,99",
+      },
+    ],
+    user: {
+      id: "eir98cr",
+      createdItems: ["kdjfdfdk"],
+    },
+  },
   mutations: {},
   actions: {},
-  getters: {},
+  getters: {
+    loadedFood(state) {
+      return state.loadedFood;
+    },
+    loadedDrinks(state) {
+      return state.loadedDrinks;
+    },
+  },
 });
