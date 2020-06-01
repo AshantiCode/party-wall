3fdc31779dc8c9b5ad82455ceac79a32de91b8cd
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-01 19:54:24 +0200

    add dynamic data injection

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,26 +1,26 @@
 <template>
   <v-container>
     <v-row>
-      <v-col md="6" offset-md="3">
-        <v-card class="mb-5">
+      <v-col sm="6" offset-sm="3">
+        <v-card v-for="(food) in food" :key="food.id" class="mb-5">
           <v-container>
             <v-row>
               <v-col cols="5">
                 <v-img :src="require('@/assets/food.jpg')"></v-img>
               </v-col>
               <v-col cols="7">
-                <v-card-title>Steak from Kobe 500gramm</v-card-title>
+                <v-card-title>{{food.name}}</v-card-title>
                 <v-card-text>
                   <v-expansion-panels accordion>
                     <v-expansion-panel>
                       <v-expansion-panel-header>Description</v-expansion-panel-header>
-                      <v-expansion-panel-content>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-expansion-panel-content>
+                      <v-expansion-panel-content>{{food.description}}</v-expansion-panel-content>
                     </v-expansion-panel>
                   </v-expansion-panels>
                 </v-card-text>
                 <div>
-                  <v-chip class="ma-2" color="secondary">3 x in Stock</v-chip>
-                  <v-chip class="ma-2" color="secondary">5 $</v-chip>
+                  <v-chip class="ma-2" color="secondary">{{food.quantity}} x in Stock</v-chip>
+                  <v-chip class="ma-2" color="secondary">{{food.price}} $</v-chip>
                 </div>
               </v-col>
             </v-row>
@@ -28,18 +28,18 @@
           </v-container>
         </v-card>
 
-        <v-card class="mb-5">
+        <v-card v-for="drink in drinks" :key="drink.id" class="mb-5">
           <v-container>
             <v-row>
               <v-col cols="5">
                 <v-img :src="require('@/assets/drinks.jpg')"></v-img>
               </v-col>
               <v-col cols="7">
-                <v-card-title>Britta Belgium Beer 0,5ml</v-card-title>
+                <v-card-title>{{drink.name}}</v-card-title>
 
                 <div>
-                  <v-chip class="ma-2" color="secondary">6 pack</v-chip>
-                  <v-chip class="ma-2" color="secondary">3 $</v-chip>
+                  <v-chip class="ma-2" color="secondary">{{drink.quantity}} pack</v-chip>
+                  <v-chip class="ma-2" color="secondary">{{drink.price}} $</v-chip>
                 </div>
               </v-col>
             </v-row>
@@ -50,3 +50,44 @@
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      food: [
+        {
+          id: "9340923842",
+          name: "Steak",
+          description: "So lecker yummy yummy",
+          quantity: "3",
+          price: "5,99"
+        },
+        {
+          id: "932429034",
+          name: "Pizza",
+          description: "So lecker yummy yummy",
+          quantity: "10",
+          price: "8,99"
+        }
+      ],
+      drinks: [
+        {
+          id: "098938789378",
+          name: "Beer",
+          description: "I am thirsty",
+          quantity: "6",
+          price: "3,99"
+        },
+        {
+          id: "87739472",
+          name: "Coca Cola",
+          description: "I am thirsty",
+          quantity: "4",
+          price: "7,99"
+        }
+      ]
+    };
+  }
+};
+</script>
