3f51e5f28e39083c25863cec27a4d5ed70c1d7f3
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-01 19:21:16 +0200

    minor change to logo name

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,13 @@
     <v-navigation-drawer app dark temporary color="primary" v-model="sideNav">
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title> Menu</v-list-item-title>
+          <v-list-item-title>Menu</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
       <v-divider></v-divider>
 
       <v-list-item-group>
-        <v-list-item
-          temporary
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.link"
-        >
+        <v-list-item temporary v-for="item in menuItems" :key="item.title" :to="item.link">
           <v-icon>{{ item.icon }}</v-icon>
           <v-list-item-content>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
@@ -24,22 +19,15 @@
     </v-navigation-drawer>
 
     <v-app-bar app dark class="text-uppercase" color="primary">
-      <v-app-bar-nav-icon
-        @click="sideNav = !sideNav"
-        class="hidden-sm-and-up"
-      ></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="sideNav = !sideNav" class="hidden-sm-and-up"></v-app-bar-nav-icon>
       <v-toolbar-title>
         <div class="d-flex align-center">
-          <v-img
-            :src="require('@/assets/logo.png')"
-            alt="Party Wall Logo"
-            width="40"
-            class="mr-5"
-          />
-          <h1>
-            <span class="font-weight-thin ">Party</span>
-            <span class="font-weight-bold">Wall</span>
-          </h1>
+          <v-img :src="require('@/assets/logo.png')" alt="Party Wall Logo" width="40" class="mr-5" />
+          <router-link to="/" tag="span" style="cursor:pointer;">
+            <h1>
+              <span class="font-weight-thin">Party</span>Wall
+            </h1>
+          </router-link>
         </div>
       </v-toolbar-title>
 
@@ -47,9 +35,7 @@
 
       <v-toolbar-items class="hidden-xs-only">
         <v-btn text v-for="item in menuItems" :key="item.title" :to="item.link">
-          <v-icon left>
-            {{ item.icon }}
-          </v-icon>
+          <v-icon left>{{ item.icon }}</v-icon>
           {{ item.title }}
         </v-btn>
       </v-toolbar-items>
@@ -71,9 +57,9 @@ export default {
       menuItems: [
         { title: "Sign In", icon: "mdi-lock-open", link: "/sign-in" },
         { title: "Sign up", icon: "mdi-face", link: "/sign-up" },
-        { title: "Add Item", icon: "mdi-plus", link: "/add-item" },
-      ],
+        { title: "Add Item", icon: "mdi-plus", link: "/add-item" }
+      ]
     };
-  },
+  }
 };
 </script>
