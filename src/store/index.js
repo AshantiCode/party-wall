e67402fb120a08908372099ab167c6e667c685a2
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-03 01:10:05 +0200

    remove reduntant food/drink functions in state / store

--- src/store/index.js
+++ src/store/index.js
@@ -13,36 +13,11 @@ export const store = new Vuex.Store({
     },
   },
   mutations: {
-    addFood(state, payload) {
-      state.loadedFood.unshift(payload);
-    },
-    addDrink(state, payload) {
-      state.loadedDrinks.unshift(payload);
-    },
     setUser(state, payload) {
       state.user = payload;
     },
   },
   actions: {
-    addFood({ commit }, payload) {
-      const newFood = {
-        name: payload.name,
-        description: payload.description,
-        weight: payload.weight,
-        quantity: payload.quantity,
-        price: payload.price,
-      };
-      commit("addFood", newFood);
-    },
-    addDrink({ commit }, payload) {
-      const newDrink = {
-        name: payload.name,
-        volume: payload.volume,
-        quantity: payload.quantity,
-        price: payload.price,
-      };
-      commit("addDrink", newDrink);
-    },
     async registerUser({ commit }, payload) {
       console.log("Payload :", payload);
       const response = await firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password);
@@ -52,14 +27,6 @@ export const store = new Vuex.Store({
         savedItems: [],
       };
       commit("setUser", newUser);
-    },
-  },
-  getters: {
-    loadedFood(state) {
-      return state.loadedFood;
-    },
-    loadedDrinks(state) {
-      return state.loadedDrinks;
     },
   },
 });
