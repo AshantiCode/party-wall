c9918ff100c263ec37f2ec1ace2be6a4ab2eb33b
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-02 17:42:28 +0200

    remove hardcoded items from state

--- src/store/index.js
+++ src/store/index.js
@@ -7,40 +7,6 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    loadedFood: [
-      {
-        id: "9340923842",
-        name: "Steak",
-        description: "So lecker yummy yummy",
-        weight: "1",
-        quantity: "3",
-        price: "5,99",
-      },
-      {
-        id: "932429034",
-        name: "Pizza",
-        description: "So lecker yummy yummy",
-        weight: "1",
-        quantity: "10",
-        price: "8,99",
-      },
-    ],
-    loadedDrinks: [
-      {
-        id: "098938789378",
-        name: "Beer",
-        volume: "5",
-        quantity: "6",
-        price: "3,99",
-      },
-      {
-        id: "87739472",
-        name: "Coca Cola",
-        volume: "3",
-        quantity: "4",
-        price: "7,99",
-      },
-    ],
     user: {
       id: "eir98cr",
       createdItems: ["kdjfdfdk"],
@@ -79,9 +45,7 @@ export const store = new Vuex.Store({
     },
     async registerUser({ commit }, payload) {
       console.log("Payload :", payload);
-      const response = await firebase
-        .auth()
-        .createUserWithEmailAndPassword(payload.email, payload.password);
+      const response = await firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password);
       const newUser = {
         id: response.user.user.uid,
         createdItems: [],
