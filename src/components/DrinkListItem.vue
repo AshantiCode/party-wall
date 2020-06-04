0c2e927262f4edce834329bcfc0f82f85e54d4cb
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-04 02:17:51 +0200

    minor chnages related to new list implementation

--- src/components/DrinkListItem.vue
+++ src/components/DrinkListItem.vue
@@ -1,19 +1,32 @@
-<template >
+<template>
   <v-card class="mb-5">
     <v-container>
       <v-row>
-        <v-col cols="5">
-          <v-img :src="require('@/assets/drinks.jpg')"></v-img>
+        <v-col>
+          <v-card-title class="list-item-heading">{{ item.name }}</v-card-title>
+
+          <v-chip class="ma-2" color="pink" label text-color="white">
+            <v-icon left>mdi-currency-usd</v-icon>
+            {{ item.price }}
+          </v-chip>
         </v-col>
-        <v-col cols="7">
-          <v-card-title>{{ item.name }}</v-card-title>
+        <v-col>
+          <v-card-text>How many items do you want?</v-card-text>
+          <v-list-item-content>
+            <v-select v-model="menge" :items="maxMenge" label="Quantity"></v-select>
+          </v-list-item-content>
           <div>
-            <v-chip class="ma-2" color="secondary">{{ item.quantity }} pack</v-chip>
-            <v-chip class="ma-2" color="secondary">{{ item.price }} $</v-chip>
+            <p style="">Total: $ {{ totalPrice }}</p>
           </div>
         </v-col>
       </v-row>
-      <v-btn class="primary">Order now!</v-btn>
+      <v-row>
+        <v-spacer></v-spacer>
+
+        <v-col>
+          <v-btn class="primary">Order now!</v-btn>
+        </v-col>
+      </v-row>
     </v-container>
   </v-card>
 </template>
@@ -21,6 +34,33 @@
 <script>
 export default {
   name: "DrinkListItem",
-  props: ["item"]
+  props: ["item"],
+  data() {
+    return {
+      menge: 0,
+      totalPrice: 0,
+    };
+  },
+  computed: {
+    maxMenge() {
+      const quantity = this.item.quantity;
+      let array = [];
+      for (let i = 0; i <= quantity; i++) {
+        array.push(i);
+      }
+      return array;
+    },
+  },
+  watch: {
+    menge(value) {
+      return (this.totalPrice = this.item.price * value);
+    },
+  },
 };
 </script>
+
+<style>
+.list-item-heading {
+  font-size: 30px;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" sm="6" offset-sm="3">
+      <v-col cols="12" sm="8" offset-sm="2">
         <div v-for="item in items" :key="item.id">
           <FoodListItem v-if="item.category == 'food'" v-bind:item="item" />
           <DrinkListItem v-else v-bind:item="item" />
