e6e3845b5c73cbe790837d34c795b4baf52c96aa
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-05 13:28:06 +0200

    json bin again new

--- Axios.js
+++ Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-  baseURL: "https://extendsclass.com/api/json-storage/bin/dcdafab",
+  baseURL: "https://api.jsonbin.io/b/5eda2aa1655d87580c444dd6",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
