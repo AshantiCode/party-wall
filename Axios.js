f2f441126a62ab9b1257d004143e11ae514097ff
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-05 14:40:06 +0200

    second

--- Axios.js
+++ Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-  baseURL: "https://api.jsonbin.io/b/5eda29f92f5fd957fda448c6",
+  baseURL: "https://api.jsonbin.io/b/5eda0eb01f9e4e578817bec0",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
