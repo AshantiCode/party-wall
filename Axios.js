77c4d3d4239dfbdcf7fe38882229584e0a48dac1
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-05 12:58:38 +0200

    last time npoint with *

--- Axios.js
+++ Axios.js
@@ -4,6 +4,6 @@ export const Axios = axios.create({
   baseURL: "https://api.npoint.io/bc86cff86119e2c35451",
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "https://deploy-preview-6--optimistic-payne-9d2d2a.netlify.app/*",
+    "Access-Control-Allow-Origin": "*",
   },
 });
