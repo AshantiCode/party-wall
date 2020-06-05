4dd16ad333f24a8debbab44c9d2a3961059b4bf0
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-05 11:07:26 +0200

    add specific origin

--- Axios.js
+++ Axios.js
@@ -2,5 +2,8 @@ import axios from "axios";
 
 export const Axios = axios.create({
   baseURL: "https://my-json-server.typicode.com/AshantiCode/server",
-  headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "https://deploy-preview-6--optimistic-payne-9d2d2a.netlify.app/",
+  },
 });
