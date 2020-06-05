072e2d7ddced5a87d4e422f2e2ea775b50f80185
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-05 08:50:45 +0200

    style text in overlay, remove  transition bug for drink item

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -7,10 +7,16 @@
             <v-img v-if="drinks" max-height="200px" :src="require('@/assets/drinks.jpg')"></v-img>
 
             <div style="position:relative;">
-              <v-fade-transition>
-                <v-overlay v-if="hover" absolute class="white--text" opacity="0.8" value="overlay">{{
-                  item.description
-                }}</v-overlay>
+              <v-fade-transition v-if="!drinks">
+                <v-overlay v-if="hover" absolute class="pa-2" opacity="0.8" value="overlay">
+                  <v-card flat style="background: transparent;" class="pa-2">
+                    <v-card-text class="subtitle-1 font-weight-bold">
+                      {{
+                      item.description
+                      }}
+                    </v-card-text>
+                  </v-card>
+                </v-overlay>
               </v-fade-transition>
               <v-img v-if="!drinks" max-height="200px" :src="require('@/assets/food.jpg')"></v-img>
             </div>
@@ -40,7 +46,10 @@
         <v-row>
           <v-col class="py-0">
             <v-card-subtitle v-if="drinks" class="py-0 text--disabled">Vol.: {{ item.volume }} liter</v-card-subtitle>
-            <v-card-subtitle v-if="!drinks" class="py-0 text--disabled">Weight: {{ item.weight }} lbs.</v-card-subtitle>
+            <v-card-subtitle
+              v-if="!drinks"
+              class="py-0 text--disabled"
+            >Weight: {{ item.weight }} lbs.</v-card-subtitle>
           </v-col>
 
           <v-col class="py-0">
@@ -54,7 +63,12 @@
           </v-col>
 
           <v-col cols="6">
-            <v-select class="text--secondary orders" dense v-model="selectedAmount" :items="maxItemsArray"></v-select>
+            <v-select
+              class="text--secondary orders"
+              dense
+              v-model="selectedAmount"
+              :items="maxItemsArray"
+            ></v-select>
           </v-col>
         </v-row>
 
@@ -83,17 +97,17 @@ export default {
   data() {
     return {
       selectedAmount: 0,
-      totalPrice: 0,
+      totalPrice: 0
     };
   },
   methods: {
     onDeleteItem(itemId) {
       const deleteUrl = `${baseUrl}/${itemId}`;
 
-      axios.delete(deleteUrl).then((response) => {
+      axios.delete(deleteUrl).then(response => {
         console.log("Axios Delete REsponse: ", response);
       });
-    },
+    }
   },
   computed: {
     maxItemsArray() {
@@ -108,7 +122,10 @@ export default {
       return this.item.category == "drinks";
     },
     userIsAuthenticated() {
-      return this.$store.getters.user !== null && this.$store.getters.user !== undefined;
+      return (
+        this.$store.getters.user !== null &&
+        this.$store.getters.user !== undefined
+      );
     },
     createdItem() {
       return this.item.creatorId;
@@ -120,13 +137,13 @@ export default {
         return false;
       }
       return this.$store.getters.user.id == createdItem;
-    },
+    }
   },
   watch: {
     selectedAmount(value) {
       return (this.totalPrice = this.item.price * value);
-    },
-  },
+    }
+  }
 };
 </script>
 
