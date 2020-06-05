aa6d48ad70bb0fc234f8493cbe0a3690173d5f49
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-05 11:37:47 +0200

    update bin id

--- Axios.js
+++ Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-  baseURL: "https://api.jsonbin.io/b/5eda0eb01f9e4e578817bec0",
+  baseURL: "https://api.jsonbin.io/b/5eda11af1f9e4e578817bf9c",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "https://deploy-preview-6--optimistic-payne-9d2d2a.netlify.app/",
