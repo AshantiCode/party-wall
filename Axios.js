e618829b576df6fca13cd35001c136f617849a4b
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-05 12:00:51 +0200

    add schema id

--- Axios.js
+++ Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-  baseURL: "https://api.jsonbin.io/b/5eda11af1f9e4e578817bf9c",
+  baseURL: "https://api.jsonbin.io/s/5eda10ea1f9e4e578817bf5d",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "https://deploy-preview-6--optimistic-payne-9d2d2a.netlify.app/",
