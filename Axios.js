99248312e9c2c15a03700235460b8341fbdb1dcf
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-05 11:22:45 +0200

    new baseUrl

--- Axios.js
+++ Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-  baseURL: "https://my-json-server.typicode.com/AshantiCode/server",
+  baseURL: "https://api.jsonbin.io/b/5eda0eb01f9e4e578817bec0",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "https://deploy-preview-6--optimistic-payne-9d2d2a.netlify.app/",
