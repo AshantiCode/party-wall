cd536d8a2ba96597949670c4fbef7aab1aa62c5b
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-05 11:04:34 +0200

    add access control

--- Axios.js
+++ Axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const Axios = axios.create({
   baseURL: "https://my-json-server.typicode.com/AshantiCode/server",
-  headers: { "Content-Type": "x-www-form-urlencoded", crossdomain: "true" },
+  headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
 });
